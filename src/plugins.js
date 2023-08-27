7d7eced08e2845ec9382dc6f84d40aea0e63e09f
CarlP <cpost38@users.noreply.github.com>
2023-08-27 17:19:10 -0500

    feat: register pg-provider with koop

--- src/plugins.js
+++ src/plugins.js
@@ -2,9 +2,13 @@ const pgProvider = require("./pg-provider");
 const outputs = [];
 const auths = [];
 const caches = [];
+
+const pgprovider = pgProvider({
+  name: "pg module"
+});
 const plugins = [
   {
-    instance: pgProvider
+    instance: pgprovider
   }
 ];
 module.exports = [...outputs, ...auths, ...caches, ...plugins];
